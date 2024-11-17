f9e608cb8e31fd60b4484812adb8a922c1413527
SavirMadan13 <136630218+SavirMadan13@users.noreply.github.com>
2024-11-17 11:31:43 -0500

    update json file

--- src/main/main.ts
+++ src/main/main.ts
@@ -26,8 +26,8 @@ console.error = () => {};
 // console.log(args);
 // const inputFilePath = args[0]; // Get the first argument
 // const inputFilePath = process.argv[1];
-// const inputFilePath = '/Users/savirmadan/Downloads/inputDataGroup.json';
-const inputFilePath = '/Users/savirmadan/Downloads/inputDataGroup3.json';
+const inputFilePath = '/Users/savirmadan/Downloads/inputDataGroup.json';
+// const inputFilePath = '/Users/savirmadan/Downloads/inputDataGroup3.json';
 // const inputFilePath =
 //   '/Users/savirmadan/Documents/inputData.json';
 class AppUpdater {
